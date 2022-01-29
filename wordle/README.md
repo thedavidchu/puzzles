489fee427470b68dfed3c03560c19ab92948a0db
David Chu <thedavidchu@gmail.com>
2022-01-29 10:15:30 -0500

    Updated wordle solver with notes that it is a heuristic solver

--- wordle/README.md
+++ wordle/README.md
@@ -3,3 +3,15 @@
 ## Licensing
 
 List of words is from https://github.com/dwyl/english-words/blob/master/words_alpha.txt, using the Unlicense as of 2022-01-28.
+
+## Notes
+1. It does not get every word in the dictionary in 6 guesses.
+2. A possible improvement would be to add a penalty for guessing duplicate letters
+3. I have never played Wordle, rather I am just guessing what the rules are based on what my colleague (June Cai) told me. Specifically, I do not know how duplicate letters in the wrong position are dealt with.
+  E.g.
+    Key word is "knoll"
+    Guess: "level". Is the reply "~---+"?
+    Guess: "lilly". Is the reply "~-~+-" or "~--+-"
+      (i.e. only 1 of the L's in the wrong place is marked as belonging in the word)?
+  Note that I defaulted to the former for ease of implementation.
+4. This game would be best solved by a decision tree using information entropy. I am not smart enough to that, so I have created this heuristic solver. Yay!
--- wordle/match_word.py
+++ wordle/match_word.py
@@ -6,9 +6,7 @@ import numpy as np
 
 
 def guess_word(text_words: List[str], word_length: int):
-    ####
-    #   Step 3: Process words (convert to lowercase), convert to number from 0..=25)
-    ####
+    # Step 3: Process words (convert to lowercase), convert to number from 0..=25)
     def word_to_int_seq(word: str) -> Tuple[int, ...]:
         assert word.isalpha()
         return tuple(ord(l) - ord("a") for l in word.lower())
@@ -16,29 +14,21 @@ def guess_word(text_words: List[str], word_length: int):
 
     words = np.array([word_to_int_seq(word) for word in text_words])
 
-    ####
-    #   Step 4: Find the frequency of each letter appearing in each position
-    ####
+    # Step 4: Find the frequency of each letter appearing in each position
     hist = np.zeros((26, word_length))
     for j, col in enumerate(words.T):
         hist[:, j], _ = np.histogram(col, 26, (0, 26))
 
-    ####
-    #   Step 5: Find the probability of each letter appearing in its place
-    #   NOTE: the "probability" should be normalized by dividing by the number of words
-    ####
+    # Step 5: Find the probability of each letter appearing in its place
+    # NOTE: the "probability" should be normalized by dividing by the number of words
     prob_letters = np.zeros_like(words)
     for j, col in enumerate(words.T):
         prob_letters[:, j] = np.take(hist[:, j], col)
 
-    ####
-    #   Step 6: Find the probability of each word
-    ####
+    # Step 6: Find the probability of each word
     prob_words = np.sum(prob_letters, axis=1)
 
-    ####
-    #   Step 7: Find the most likely word
-    ####
+    # Step 7: Find the most likely word
     def int_seq_to_word(seq: Tuple[int, ...]) -> str:
         return "".join(chr(s + ord("a")) for s in seq)
 
@@ -48,9 +38,6 @@ def guess_word(text_words: List[str], word_length: int):
     return most_likely_word
 
 
-####
-#   Step 8: Filter words and repeat
-####
 def update_pattern(old_pattern: List[Set[str]], old_required: str, guess: str, reply: str):
     """
     Note: we apply the filter every time, so we don't need to check for the old pattern/old requirements
@@ -112,32 +99,30 @@ def test_word(key: str, guess: str):
     return "".join(reply_list)
 
 
-def main():
+def test():
     KEY_WORD = "knoll"
-    
-    ####
-    #   Step 1: Load words (separated by "\n")
-    ####
     DICTIONARY_PATH = "words_length_5.txt"
+    WORD_LENGTH = 5
+    
+    # Step 1: Load words (separated by "\n")
     with open(DICTIONARY_PATH) as f:
         all_text_words = f.read().split("\n")
 
-    ####
-    #   Step 2: Major Filters (length, ascii)
-    ####
-    WORD_LENGTH = 5
+    # Step 2: Major Filters (length, ascii)
     filtered_text_words = [word for word in all_text_words if len(word) == WORD_LENGTH and word.isalpha()]
 
+    # Step 3: Set up for repeated guessing
     text_words = filtered_text_words    # Reset every iteration
     pattern = [{l for l in "abcdefghijklmnopqrstuvwxyz"} for _ in range(WORD_LENGTH)]
     required = ""
-
     for i in range(6):
+        # Step 4: Guess most likely word
         most_likely_word = guess_word(text_words, WORD_LENGTH)
         print(f"\tGuess: {most_likely_word}")
         reply = test_word(KEY_WORD, most_likely_word)
         if "-" not in reply and "~" not in reply:
             break
+        # Step 5: Filter words based on reply and repeat
         pattern, required = update_pattern(pattern, required, most_likely_word, reply)
         text_words = apply_filter(text_words, pattern, required)
     else:
@@ -145,6 +130,52 @@ def main():
         return False
     print("SUCCESS!")
     return True
+
+
+def main():
+    DICTIONARY_PATH = "words_length_5.txt"
+    WORD_LENGTH = 5
+
+    # Step 1: Load words (separated by "\n")
+    with open(DICTIONARY_PATH) as f:
+        all_text_words = f.read().split("\n")
+
+    # Step 2: Major Filters (length, ascii)
+    filtered_text_words = [word for word in all_text_words if len(word) == WORD_LENGTH and word.isalpha()]
+
+    # Step 3: Set up for repeated guessing
+    text_words = filtered_text_words    # Reset every iteration
+    pattern = [{l for l in "abcdefghijklmnopqrstuvwxyz"} for _ in range(WORD_LENGTH)]
+    required = ""
+    for i in range(6):
+        # Step 4: Guess most likely word
+        most_likely_word = guess_word(text_words, WORD_LENGTH)
+        print(f"Guess {i + 1}: {most_likely_word}")
+        
+        reply = input("What is the reply? (Type 'help' for instructions): ")
+        while reply == "help" or len(reply) != WORD_LENGTH or not all(map(lambda c: c in "+~-", reply)):
+            print()
+            print("Instructions")
+            print("============")
+            print(f"Enter a {WORD_LENGTH} character string using the characters {{'+', '~', '-'}} where")
+            print("    '+' implies correct letter and position,")
+            print("    '~' means correct letter but wrong position, and")
+            print("    '-' means neither correct letter nor correct position.")
+            print("Note that if the keyword is 'knoll' but you were to guess 'lilly', the response would be '~-~+-' because it does not count the number of correct letters")
+            print()
+            reply = input("What is the reply? (Type 'help' for instructions)")
+
+        if reply == "+" * WORD_LENGTH:
+            break
+        # Step 5: Filter words based on reply and repeat
+        pattern, required = update_pattern(pattern, required, most_likely_word, reply)
+        text_words = apply_filter(text_words, pattern, required)
+    else:
+        print(f"YOU FAILED ON {KEY_WORD}!")
+        return False
+    print("SUCCESS!")
+    return True
+    
 
 
 if __name__ == "__main__":
