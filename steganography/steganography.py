bfc3a86cd5c804ae4d0a7eb1634e77a65b42d734
David Chu <thedavidchu@gmail.com>
2022-06-23 22:03:38 -0400

    added argparse to steganography

--- steganography/steganography.py
+++ steganography/steganography.py
@@ -1,5 +1,7 @@
-import cv2
+import argparse
 import os
+
+import cv2
 
 
 # Bit shift constants
@@ -8,10 +10,13 @@ VISIBLE_MASK_CONST = int(BIT_SHIFT * '1' + (8 - BIT_SHIFT) * '0', 2)
 
 
 def combine_visible_and_hidden(
-        visible_image_path: str,
-        hidden_image_path: str,
-        steganograph_image_path: str,
+    visible_image_path: str,
+    hidden_image_path: str,
+    steganograph_output_image_path: str,
 ):
+    assert os.path.exists(visible_image_path)
+    assert os.path.exists(hidden_image_path)
+    
     # Open input images and resize visible to hidden size
     visible_image = cv2.imread(visible_image_path)
     hidden_image = cv2.imread(hidden_image_path)
@@ -27,28 +32,35 @@ def combine_visible_and_hidden(
     steganograph = visible_image + hidden_image
 
     # Save in lossless format
-    pre, ext = os.path.splitext(steganograph_image_path)
+    pre, ext = os.path.splitext(steganograph_output_image_path)
     cv2.imwrite(pre + '.png', steganograph)
 
 
 def extract_hidden_image(
-        steganograph_image_path,
-        hidden_image_path
+    steganograph_image_path,
+    hidden_output_image_path
 ):
+    assert os.path.exists(steganograph_image_path)
+    
     # Open steganograph and shift; save it to hidden path
     steganograph_image = cv2.imread(steganograph_image_path)
     hidden_image = (steganograph_image << BIT_SHIFT)
-    cv2.imwrite(hidden_image_path, hidden_image)
+    cv2.imwrite(hidden_output_image_path, hidden_image)
 
     # Save in lossless format
-    pre, ext = os.path.splitext(hidden_image_path)
+    pre, ext = os.path.splitext(hidden_output_image_path)
     cv2.imwrite(pre + '.png', hidden_image)
 
 
 if __name__ == '__main__':
-    # Deployment Decoding Pipeline
-    deployment_decoding_pipeline = True
-    if deployment_decoding_pipeline:
-        steganograph_image_path = input('Where is the encoded file?')
-        result_image_path = input('Where do you want to save the hidden message?')
-        extract_hidden_image(steganograph_image_path, result_image_path)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--visible-path", type=str, required=False, default=None)
+    parser.add_argument("--hidden-path", type=str, required=True)
+    parser.add_argument("--steganograph-path", type=str, required=True)
+    args = parser.parse_args()
+
+    if args.visible_path is None:   # Visible path is only specified for encoding
+        extract_hidden_image(args.steganograph_path, args.hidden_path)
+    else:
+        combine_visible_and_hidden(args.visible_path, args.hidden_path, args.steganograph_path)
+
