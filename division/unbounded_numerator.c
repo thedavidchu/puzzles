f74a262d3e81832141564c6c0ef51c75528a8158
David Chu <45278895+thedavidchu@users.noreply.github.com>
2023-06-06 16:11:54 -0400

    Create unbounded_numerator.c
    Original description by Michael Guerzhoy:
    
    The file file.txt contains a decimal number of unknown size n (potentially d digits, where d could be anything). Write a function to read in that number n and print n/190, using integer division. Use C.  You may not make any assumptions about the size of the file. You may use LLM-based tools to help you.

---
+++ division/unbounded_numerator.c
@@ -0,0 +1,61 @@
+/** Divide an arbitrarily large positive decimal integer (in "file.txt") by 190.
+ * More generally, we could divide by any positive integer that is no larger than
+ * a tenth of the maximum size_t.
+ *
+ * I am proud of the fact that this utilizes streaming to have constant space
+ * complexity. I almost implemented a full BigInteger library (partially for fun)
+ * to complete this challenge. This is much simpler. Both the input and output are
+ * streamed immediately.
+ *
+ * Created for my application as an ESC180 2023/CSC190 2024 TA.
+ *
+ * Requires C99 or higher! This is because we print size_t.
+ * 
+ * NOTE: I do not support negative numbers. The file must only contain the
+ * characters 0 to 9. A blank file will evaluate to 0.
+ */
+
+#include <stdbool.h>
+#include <assert.h>
+#include <stdio.h>
+
+#define FILE_NAME "file.txt"
+
+int main() {
+    int err = 0;
+    int is_zero = true;
+    size_t remainder = 0;
+    const size_t divisor = 190;
+
+    /* Open file */
+    FILE *fp = fopen(FILE_NAME, "r");
+    assert(fp && "error opening file");
+
+    for (int c = fgetc(fp); c != EOF; c = fgetc(fp)) {
+        /* Assume digits char are ascending, consecutive numbers from 0 to 9 */
+        assert('0' <= c && c <= '9' && "invalid decimal character!");
+        remainder = 10 * remainder + (c - '0');
+        if (remainder >= divisor) {
+            size_t quotient = remainder / divisor;
+            remainder -= quotient * divisor;
+            printf("%zu", quotient);
+            is_zero = false;
+        } else {
+            if (is_zero) {
+                /* Skip leading zeros from being displayed. This could be an
+                 * else-if clause, but I feel this is clearer. */
+                continue;
+            }
+            printf("0");
+        }
+    }
+    if (is_zero) {
+        printf("0");
+    }
+    printf("\n");
+
+    /* Close file */
+    err = fclose(fp);
+    assert(!err && "error closing");
+    return 0;
+}
